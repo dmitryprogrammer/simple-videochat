748c5b1bab6b039d7e59b7c359028537d2f14165
dmitryprogrammer <dpischalka@gmail.com>
2025-06-30 17:16:29 +0300

    updated server/index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,17 @@
 import * as express from "express";
+import * as ws from "ws";
 
 const PORT = process.env.PORT ?? 3000;
 
 const app = express();
-
-let counter = 0;
+const socket = new ws.Server({ port: 8080 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
 
-  setInterval(() => {
-    counter++;
-    console.log(counter);
-  }, 100);
+socket.on("connection", (socketItem: ws) => {
+  socketItem.on("message", (message: ws) => {
+    console.log(message);
+  });
 });
